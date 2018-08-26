3afe4507cce879cdb9e8f5ab80e314d0343c0ad2
Karlos Oliveira <karlos.1914.so@gmail.com>
2018-08-26 19:13:05 -0300

    icons

--- include/menu.php
+++ include/menu.php
@@ -1,5 +1,5 @@
 <!-- Dropdown Structure -->
-<ul id="dropdown1" class="dropdown-content">
+<!--<ul id="dropdown1" class="dropdown-content">
   <li><a href="#!" class="red-text">Fluxo de caixa</a></li>
   <li><a href="#!" class="red-text"></a></li>
 </ul>
@@ -11,8 +11,34 @@
       <li><a href="entrada.php">Entrada</a></li>
       <li><a href="saida.php">Saída</a></li>
       <li><a href="cadastroCliente.php">Clientes</a></li>
-      <!-- Dropdown Trigger -->
+      <!-- Dropdown Trigger --
       <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Relatórios<i class="material-icons right">arrow_drop_down</i></a></li>
     </ul>
+
   </div>
-</nav>
+</nav>-->
+<head>
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+</head>
+<nav class="red lighten-2">
+    <div class="nav-wrapper">
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fas fa-bars fa-lg"></i></a>
+      <ul class="right hide-on-med-and-down hide-on-med-only">
+        <li><a href="administrativo.php"><i class="fas fa-home fa-lg"></i> Inicio</a></li>
+        <li><a href="entrada.php">Entrada</a></li>
+        <li><a href="saida.php">Saída</a></li>
+        <li><a href="cadastroCliente.php">Clientes</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <br>
+      <div class="row col s12 center-align">
+        <img class="responsive-img" width="100" src="img/logo.png">
+      </div>
+      <li><a href="administrativo.php"><i class="fas fa-home fa-lg"></i>Inicio</a></li>
+      <li><a href="entrada.php"><i class="fas fa-file-import fa-lg"></i>Entrada</a></li>
+      <li><a href="saida.php"><i class="fas fa-piggy-bank fa-lg"></i>Saída</a></li>
+      <li><a href="cadastroCliente.php"><i class="fas fa-user fa-lg"></i>Clientes</a></li>
+  </ul>
