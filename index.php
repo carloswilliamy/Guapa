a4af341bb14be3bd4bd5f8fd7611802da30c7025
Carlos Williamy <=>
2018-08-27 17:26:53 -0300

    Cadastro Clientes

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="br">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Guapa</title>
-
-  <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link rel="icon" href="img/logo2.png">
-</head>
+    <meta charset="utf-8">
+    <title>Guapa</title>
+    <!--Import Google Icon Font-->
+    
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+    <!--Import materialize.css-->
+    
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+	<link rel="icon" href="img/logo2.png">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
 <body onload="$('#usuario').select();">
-    <br><br><img id="logo2" class='center responsive-img' width="150" src="img/logo.png">
-    <div class="container col s4 m2 l2">
+    <br><br><img class='responsive-img col s12 m6 l2' width="150" src="img/logo.png">
+    <div class="container">
     <form name="login" class="col s12" action="DAO.php" method="POST">
-      <div class="row">
+      <div class="row col s12 m2 l2">
         <div class="input-field col s12">
           <i class="material-icons prefix">person_outline</i>
           <input id="icon_prefix" type="text" name="usuario"class="validate">
@@ -29,10 +32,10 @@
           <label for="icon_prefix2">Senha</label>
         </div>
       </div>
-      <button class="red right btn waves-effect waves-light" type="submit" name="action" onclick="return loginTemp()">Entrar
+      <button class="btn waves-effect waves-light red lighten-2 right col s12 m6 l2" type="submit" name="action" onclick="return loginTemp()">Entrar
         <i class="material-icons right">send</i>
       </button>
-      <a id="C2" class='azulColor'href="cadastro.php">Esqueceu sua senha?</a>
+      <a class='waves-effect waves-teal btn-flat left col s12 m6 l2' href="cadastro.php">Esqueceu sua senha?</a>
     </form>
   </div>
   <br>
@@ -45,9 +48,12 @@
 
 
   <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/script.js"></script>
+   <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/jquery.maskedinput.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
 
   </body>
 </html>
